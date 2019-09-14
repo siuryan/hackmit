403c6dad31554be0068887a095d13aa3fd9b6fa3
Ryan Siu <rsiu05@gmail.com>
2019-09-14 17:36:55 -0400

    Bug fixes

--- app.py
+++ app.py
@@ -9,12 +9,10 @@ from flask import Flask, request
 quandl.ApiConfig.api_key = "oSysYNKB7dqj4asAuDQy"
 indicator_to_code = pd.read_csv("indicators_table.csv")
 
-
 app = Flask(__name__)
 
-
-def get_indicator_code(input_data_type):
-    return indicator_to_code['Code'][list(np.where((indicator_to_code['Indicator']==input_data_type)==True))[0][0]]
+# def get_indicator_code(input_data_type):
+#     return indicator_to_code['Code'][list(np.where((indicator_to_code['Indicator']==input_data_type)==True))[0][0]]
 
 def get_quandl_data(input_zipcode, code, from_date, to_date):
     return quandl.get('ZILLOW/Z{}_{}'.format(input_zipcode, code), start_date=from_date, end_date=to_date)
@@ -28,10 +26,10 @@ def plot_time_series(series, input_data_type, input_zipcode):
     pyplot.xlabel("Time", fontsize="x-large", weight="bold")
     pyplot.ylabel(input_data_type, fontsize="x-large", weight="bold")
     pyplot.title("{} for area {}".format(input_data_type, input_zipcode))
-    return mpld3.fig_to_html(fig, template_type="simple")
 
-
-
+    html = mpld3.fig_to_html(fig, template_type="simple")
+    print (html)
+    return html
 
 @app.route('/')
 def home():
@@ -62,12 +60,12 @@ def home():
 def data():
     zip_code = request.args.get('zip_code')
     indicator = request.args.get('indicator')
-    code = get_indicator_code(indicator)
+    # code = get_indicator_code(indicator)
     from_date = request.args.get('start_date')
     to_date = request.args.get('end_date')
     # from_date_string = from_date.strftime('%Y-%m-%d')
     # to_date_string = to_date.strftime('%Y-%m-%d')
-    return plot_time_series(get_quandl_data(zip_code, code, from_date, to_date), indicator, zip_code)
+    return plot_time_series(get_quandl_data(zip_code, indicator, from_date, to_date), indicator, zip_code)
 
     # return str(quandl.get('ZILLOW/Z{}_{}'.format(zip_code, indicator), start_date=now_string, end_date=now_string))
 
