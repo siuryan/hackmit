5070e78df2b2214a4749be5c7d762bd1f3b40cdb
ssundaram21 <shobhita.saroja@gmail.com>
2019-09-14 17:54:06 -0400

    alex stuff

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import datetime
 from matplotlib import pyplot
 import mpld3
 from flask import Flask, request
+from bs4 import BeautifulSoup as BS
+import requests
+import re
+
 
 quandl.ApiConfig.api_key = "oSysYNKB7dqj4asAuDQy"
 indicator_to_code = pd.read_csv("indicators_table.csv")
@@ -30,6 +34,27 @@ def plot_time_series(series, input_data_type, input_zipcode):
     html = mpld3.fig_to_html(fig, template_type="simple")
     print (html)
     return html
+
+def get_school_link(zipcode):
+    zipcode = "07871"
+    state = 'NJ'
+    text = requests.get('https://nces.ed.gov/ccd/districtsearch/district_list.asp?Search=1&details=1&InstName=&DistrictID=&Address=&City=&State=&Zip=' +  zipcode + '&Miles=&County=&PhoneAreaCode=&Phone=&DistrictType=1&DistrictType=2&DistrictType=3&DistrictType=4&DistrictType=5&DistrictType=6&DistrictType=7&DistrictType=8&NumOfStudents=&NumOfStudentsRange=more&NumOfSchools=&NumOfSchoolsRange=more')
+    soup = BS(text.content, 'html5lib') 
+
+    links = []
+    for link in soup.findAll('a', attrs={'href': re.compile("district_detail")}):
+        links.append('https://nces.ed.gov/ccd/districtsearch/' + link.get('href'))
+
+    finallink = links[0]
+    text2 = requests.get(finallink)
+
+    soup2 = BS(text2.content, 'html5lib') 
+
+    links2 = []
+    for link in soup2.findAll('a', attrs={'href': re.compile("/Programs/Edge")}):
+        links2.append('https://nces.ed.gov' + link.get('href'))
+        
+    return links2[0]
 
 @app.route('/')
 def home():
@@ -60,12 +85,12 @@ def home():
 def data():
     zip_code = request.args.get('zip_code')
     indicator = request.args.get('indicator')
-    # code = get_indicator_code(indicator)
     from_date = request.args.get('start_date')
     to_date = request.args.get('end_date')
-    # from_date_string = from_date.strftime('%Y-%m-%d')
-    # to_date_string = to_date.strftime('%Y-%m-%d')
-    return plot_time_series(get_quandl_data(zip_code, indicator, from_date, to_date), get_indicator(indicator), zip_code)
+
+    link = get_school_link(zip_code)
+
+    return plot_time_series(get_quandl_data(zip_code, indicator, from_date, to_date), get_indicator(indicator), zip_code) + "<p>School: {}</p>".format(link)
 
 
 if __name__ == '__main__':
