5fc7afd33ac174a006088cd852429991f1633597
Ryan Siu <rsiu05@gmail.com>
2019-09-14 16:44:13 -0400

    Add radio buttons for indicators

--- app.py
+++ app.py
@@ -9,13 +9,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '''
+    indicators = {
+        'MLPAH': 'Median Listing Price - All Homes',
+        'MRPAH': 'Median Rental Price - All Homes',
+        'MSPAH': 'Median Sold Price - All Homes',
+        'ZHVIAH': 'Zillow Home Value Index - All Homes',
+        'ZRIAH': 'Zillow Rental Index - All Homes'
+    }
+    html = '''
     <form action="/data">
     Zip Code: <input type="text" name="zip_code"><br>
-    Indicator Code: <input type="text" name="indicator"><br>
+    Indicators:<br>
+    '''
+
+    html += '\n'.join(['<input type="radio" name="indicator" value=' + i + '>' + indicators[i] + '<br>' for i in indicators])
+
+    html += '''
     <input type="submit" value="Submit">
     </form>
     '''
+    return html
 
 @app.route('/data')
 def data():
@@ -27,7 +40,12 @@ def data():
     five_y_ago = now - datetime.timedelta(days=365*5)
     five_y_ago_string = five_y_ago.strftime('%Y-%m-%d')
 
-    return str(quandl.get('ZILLOW/Z{}_{}'.format(zip_code, indicator), start_date=five_y_ago_string, end_date=now_string))
+    try:
+        ret_data = str(quandl.get('ZILLOW/Z{}_{}'.format(zip_code, indicator), start_date=five_y_ago_string, end_date=now_string))
+    except:
+        ret_data = "Could not get data for input parameters"
+
+    return ret_data
 
 if __name__ == '__main__':
     app.run()
